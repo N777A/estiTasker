e6998b8f83d58312235b3acfefd0b9e82477bfc5
Na <na@Mac.local>
2023-10-19 14:28:47 +0900

    project_controller index編集

--- task-backend/app/controllers/projects_controller.rb
+++ task-backend/app/controllers/projects_controller.rb
@@ -2,17 +2,8 @@ class ProjectsController < ApplicationController
   before_action :authenticate_api_v1_user!
 
   def index
-    puts "UID: #{request.headers['uid']}"
-    puts "Access-Token: #{request.headers['access-token']}"
-    puts "Client: #{request.headers['HTTP_CLIENT']}"
-    request.headers.each do |key, value|
-      puts "#{key}: #{value}"
-    end
-    puts '________________________________________________________'
-    Rails.logger.info "Current User: #{current_api_v1_user.inspect}"
-
     @user = current_api_v1_user
-    @projects = Project.all
+    @projects = @user.projects.all
     
     render json: { user: @user, projects: @projects}
   end
