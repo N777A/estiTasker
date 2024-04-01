53bc5c7aab89deb0ea0fb544dec49e4b80eeb951
Na <na@Mac.local>
2024-04-01 15:30:53 +0900

    corsにフロントエンドのURLを追加

--- task-backend/config/initializers/cors.rb
+++ task-backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'localhost:3001', 'esti-tasker.vercel.app'
 
     resource '*',
       headers: :any,
