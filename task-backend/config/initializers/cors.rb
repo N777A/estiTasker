7c4ad4f44bebdb76f0310de63c33e4a7699e65a3
Na <na@Mac.local>
2024-04-01 15:59:28 +0900

    cors編集

--- task-backend/config/initializers/cors.rb
+++ task-backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'https://estitasker-backend-bbd0841d1395.herokuapp.com/'
+    origins 'localhost:3001', 'https://esti-tasker.vercel.app/'
 
     resource '*',
       headers: :any,
