f5ba0a3c82e31caab362ccc986ece2983855965f
Na <na@Mac.local>
2024-04-03 14:44:29 +0900

    初期ページを編集

--- task_frontend/src/pages/index.tsx
+++ task_frontend/src/pages/index.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/sign_in')
+  }, [router]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/projects">Projects</Link>
-    </main>
+    <div>Loading...</div>
   )
 }
