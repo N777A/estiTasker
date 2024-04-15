566d3183ac5b4ea61c8a893df2f7c31ded717868
Na <na@Mac.local>
2024-04-15 20:11:26 +0900

    Claud v1からv３ haikuに変更

--- task-backend/app/controllers/api/v1/llm_controller.rb
+++ task-backend/app/controllers/api/v1/llm_controller.rb
@@ -4,19 +4,37 @@ class Api::V1::LlmController < ApplicationController
 
   def create_tasks
     @tasks = invoke(
-      "Break down to Tasks based on description. Estimate Time for each task. Output in Japanese.",
-      params[:input]
+      "Break down to Tasks based on description. Estimate Time for each task.
+      Follow output-format and wrap result in <output></output> tag. output in Japanese.
+      <description>
+      #{params[:input]}
+      </description>
+      <output-format>
+      [
+        { \"title\": string, \"description\": string, \"estimated_time\": string (format: \"HH:mm\") }
+      ]
+      <output-format>
+      Breakdown task into small tasks. For example if it is frontend, break tasks to page, feature, and component level."
     )
+    
     render json: { tasks: @tasks }
   end
-
-  def estimate_task_time
-    @task = invoke(
-      "Estimate the time it will take to complete the following task in hour.",
-      params[:task]
-    )
-    render json: { task: @task }
-  end
+  
+  # def estimate_task_time
+  #   @task = invoke(
+  #     "Human: Estimate the time it will take to complete the following task in hour. 
+  #     Follow output-format and wrap result in <output></output> tag.
+  #     <task>
+  #     #{params[:task]}
+  #     </task>
+  #     <output-format>
+  #     { \"title\": string, \"time\": number (time to coplete in hour) }
+  #     </output-format>
+  #     Assistant:"
+  #   )
+    
+  #   render json: { task: @task }
+  # end
 
   private
 
@@ -24,35 +42,30 @@ class Api::V1::LlmController < ApplicationController
     @client = Aws::BedrockRuntime::Client.new()
   end
 
-  def invoke(description, input)
-    messages = [{
-      "role": "user",
-      "content": [{"type": "text", "text": input}]
-    }]
-
-    body = {
-      "anthropic_version": "bedrock-2023-05-31",
-      "max_tokens": 300,
-      "system": "あなたは優秀なAIボットです",
-      "messages": messages
-    }
-
-    response = @client.invoke_model(
-      model_id: "anthropic.claude-3-haiku-20240307-v1:0",
-      body: body.to_json,
+  def invoke(prompt)
+    resp = @client.invoke_model({
+      body: {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 1024,
+        "messages": [
+            {
+              "role": "user",
+              "content": [
+                {"type": "text", "text": prompt}
+              ],
+            }
+        ],
+    }.to_json,
       content_type: "application/json",
-      accept: "application/json"
-    )
+      accept: "application/json",
+      model_id: "anthropic.claude-3-haiku-20240307-v1:0",
+    })
+    parsed_body = JSON.parse(resp.body.read)
+    puts parsed_body
+    completion = parsed_body['content'][0]['text']
+    puts completion.match(/<output>(.*?)<\/output>/m)
+    json_string = completion.match(/<output>(.*?)<\/output>/m)[1]
+    JSON.parse(json_string)
 
-    response_body = JSON.parse(response.body.read)
-    completion = response_body["completion"]
-    if completion
-      match_data = completion.match(/<output>(.*?)<\/output>/m)
-      match_data ? JSON.parse(match_data[1]) : {}
-    else
-      { error: "No valid output received from the model." }
-    end
-  rescue => e
-    { error: e.message }
   end
 end
